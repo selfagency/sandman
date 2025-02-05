278066c7a5d9694406ec44bf8dbf62a75d37bbf4
daniel sieradski <daniel@self.agency>
2025-02-05 18:47:25 -0500

    the known unknowns

--- scripts/common.ts
+++ scripts/common.ts
@@ -1,11 +1,11 @@
-async function transport(level: string, message: string, rest: string[]) {
+async function transport(level: string, message: string, rest: unknown[]) {
   try {
     await fetch(`http://localhost:${process.env.PORT || 3000}/log`, {
-      method: 'POST',
-      body: JSON.stringify({ message, level, rest }),
+      body: JSON.stringify({ level, message, rest }),
       headers: {
         Authorization: `Basic ${btoa(':' + process.env.LOG_KEY)}`,
       },
+      method: 'POST',
     });
 
     return { message, success: true };
@@ -16,7 +16,15 @@ async function transport(level: string, message: string, rest: string[]) {
 }
 
 const log = {
-  error(message: string, ...rest: string[]) {
+  debug(message: string, ...rest: unknown[]) {
+    const script = process.argv[1]
+      .split('/')
+      .pop()
+      ?.replace(/\.[jt]s/, '') as string;
+    const logMessage = `[${script}] ${message}`;
+    transport('debug', logMessage, rest).then(() => console.debug(logMessage, ...rest));
+  },
+  error(message: string, ...rest: unknown[]) {
     const script = process.argv[1]
       .split('/')
       .pop()
@@ -24,15 +32,7 @@ const log = {
     const logMessage = `[${script}] ${message}`;
     transport('error', logMessage, rest).then(() => console.error(logMessage, ...rest));
   },
-  warn(message: string, ...rest: string[]) {
-    const script = process.argv[1]
-      .split('/')
-      .pop()
-      ?.replace(/\.[jt]s/, '') as string;
-    const logMessage = `[${script}] ${message}`;
-    transport('warn', logMessage, rest).then(() => console.warn(logMessage, ...rest));
-  },
-  info(message: string, ...rest: string[]) {
+  info(message: string, ...rest: unknown[]) {
     const script = process.argv[1]
       .split('/')
       .pop()
@@ -40,13 +40,13 @@ const log = {
     const logMessage = `[${script}] ${message}`;
     transport('info', logMessage, rest).then(() => console.info(logMessage, ...rest));
   },
-  debug(message: string, ...rest: string[]) {
+  warn(message: string, ...rest: unknown[]) {
     const script = process.argv[1]
       .split('/')
       .pop()
       ?.replace(/\.[jt]s/, '') as string;
     const logMessage = `[${script}] ${message}`;
-    transport('debug', logMessage, rest).then(() => console.debug(logMessage, ...rest));
+    transport('warn', logMessage, rest).then(() => console.warn(logMessage, ...rest));
   },
 };
 

f71e2e5a86d2c1bd85f527dec1475e8a4bb6ce6e
daniel sieradski <daniel@self.agency>
2025-02-05 19:30:57 -0500

    bah

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-corepack enable
-cd "/app/scripts" || exit
-if [ -f "package.json" ]; then
-  pnpm i
-fi
-
 cd "/app" || exit
 node --experimental-specifier-resolution=node "dist/index.js"

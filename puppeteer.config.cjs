6193327a101b747c2d03fa63e95cd8b4063371b7
Daniel Sieradski <daniel@self.agency>
2025-02-05 23:19:57 -0500

    perhaps

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -1,7 +1,4 @@
 /* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-require-imports */
-const { dirname, join } = require('path');
-
 module.exports = {
-  cacheDirectory: join(dirname(), '.puppeteer'),
+  cacheDirectory: `${__dirname}/.puppeteer`,
 };
